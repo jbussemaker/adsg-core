8e4e32d8115f83aecbd96b558a5d8233530e3c5d
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2024-05-07 17:45:29 +0200

    Fix premature start node definition bug

--- adsg_core/tests/test_graph.py
+++ adsg_core/tests/test_graph.py
@@ -832,3 +832,16 @@ def test_sel_choice_scenarios(n):
     assert scs[0].n_opts == 2
     assert len(scs[0].opt_idx_combinations) == 4
     assert np.all(scs[0].opt_idx_combinations[0] == np.array([[0, 1]]).T)
+
+
+def test_early_start_node_def(n):
+    adsg = BasicADSG()
+    adsg.add_edges([
+        (n[0], n[1]), (n[2], n[3]),
+    ])
+    adsg = adsg.set_start_nodes({n[0], n[2]})
+
+    c1 = adsg.add_selection_choice('C1', n[0], n[4:6])
+    c2 = adsg.add_selection_choice('C2', n[3], n[6:8])
+
+    assert adsg.get_ordered_next_choice_nodes() == [c1, c2]
